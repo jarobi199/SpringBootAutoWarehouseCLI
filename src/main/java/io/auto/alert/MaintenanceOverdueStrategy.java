6be436c33a962cc137bee2faebff673d3b90ae01
ventrya <dpryce@ventrya.com>
2026-06-26 18:20:58 -0400

    Another commit.

--- src/main/java/io/auto/alert/MaintenanceOverdueStrategy.java
+++ src/main/java/io/auto/alert/MaintenanceOverdueStrategy.java
@@ -1,12 +1,24 @@
 package io.auto.alert;
 
+import io.auto.bridge.SpringContext;
+import io.auto.enums.AlertType;
 import io.auto.interfaces.AlertStrategy;
+import io.auto.model.MaintenanceRecord;
 import io.auto.model.Vehicle;
+import io.auto.service.MaintenanceService;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 
 public class MaintenanceOverdueStrategy implements AlertStrategy {
+
+    private final MaintenanceService maintenanceService;
+
+    public MaintenanceOverdueStrategy() {
+        maintenanceService = SpringContext.getBean(MaintenanceService.class);
+    }
+
     @Override
     public boolean supports(Vehicle vehicle) {
         return (vehicle != null);
@@ -15,8 +27,20 @@ public class MaintenanceOverdueStrategy implements AlertStrategy {
     @Override
     public AlertResult evaluate(Vehicle vehicle) {
         AlertResult alertResult = null;
-        long vehicleAge = ChronoUnit.YEARS.between(vehicle.getPurchaseDate(), LocalDate.now());
 
+        long vehicleAge = ChronoUnit.YEARS.between(vehicle.getPurchaseDate(), LocalDate.now());
+        MaintenanceRecord maintenanceRecord = maintenanceService.findByVehicleId(vehicle.getId())
+                .stream().max(Comparator.comparing(MaintenanceRecord::getServiceDate)).orElse(null);
+
+        if(((maintenanceRecord == null) && (vehicleAge > 1)) || ( (maintenanceRecord != null)  && maintenanceRecordAgeGreaterThanOne(maintenanceRecord))) {
+            alertResult = new AlertResult(vehicle, AlertType.MAINTENANCE_OVERDUE, (maintenanceRecord == null) ? "This vehicle has not been serviced!" : "The last service date of this vehicle was" + maintenanceRecord.getServiceDate().toString());
+        }
         return alertResult;
     }
+
+    private boolean maintenanceRecordAgeGreaterThanOne(MaintenanceRecord maintenanceRecord) {
+        long maintenanceRecordAge = ChronoUnit.YEARS.between(maintenanceRecord.getServiceDate(), LocalDate.now());
+        return (maintenanceRecordAge > 1);
+    }
+
 }
--- src/main/java/io/auto/service/MaintenanceService.java
+++ src/main/java/io/auto/service/MaintenanceService.java
@@ -1,7 +1,20 @@
 package io.auto.service;
 
+import io.auto.model.MaintenanceRecord;
+import io.auto.repository.MaintenanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MaintenanceService {
+
+    @Autowired
+    private MaintenanceRepository maintenanceRepository;
+
+    public List<MaintenanceRecord> findByVehicleId(String vehicleId) {
+        return maintenanceRepository.findByVehicleId(vehicleId);
+    }
+
 }
