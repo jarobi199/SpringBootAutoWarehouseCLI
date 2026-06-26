44dc9fde1a675cedbb92126e7f2e436a547d394e
derrickpryce <dpryce7@gmail.com>
2026-06-26 01:31:12 -0400

    Another commit.

---
+++ src/main/java/io/auto/repository/MaintenanceRepository.java
@@ -0,0 +1,11 @@
+package io.auto.repository;
+
+import io.auto.model.MaintenanceRecord;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface MaintenanceRepository extends MongoRepository<MaintenanceRecord, String> {
+    List<MaintenanceRecord> findByVehicleId(String vehicleId);
+    List<MaintenanceRecord> findByUserIdOrderByServiceDateDesc(String userId);
+}
