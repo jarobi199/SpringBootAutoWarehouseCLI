d587a785687968dc531aaefaec3318b04f5e89e5
ventrya <dpryce@ventrya.com>
2026-06-30 21:44:50 -0400

    Another commit.

--- src/main/java/io/auto/menu/MainMenu.java
+++ src/main/java/io/auto/menu/MainMenu.java
@@ -31,7 +31,7 @@ public class MainMenu implements IMenu {
 
         System.out.println();
         displayTitle();
-        authenticateMenu.automaticLogin();
+        authenticateMenu.show();
         System.out.println();
 
         do {
